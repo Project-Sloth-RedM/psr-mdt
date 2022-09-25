3ec818948127096947f2c06a11c01b2e0bafea14
MoneSuper <62680426+MoneSuper@users.noreply.github.com>
2022-09-25 05:28:59 +0300

    psr-mdt?

---
+++ fxmanifest.lua
@@ -0,0 +1,38 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'Flawws, Flakey, Idris and the Project Sloth team'
+description 'EchoRP MDT Rewrite for QBCore'
+version '0.9.9'
+
+lua54 'yes'
+
+shared_script 'shared/config.lua'
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/utils.lua',
+    'server/dbm.lua',
+    'server/main.lua'
+}
+client_scripts{
+    'client/main.lua',
+    'client/cl_impound.lua'
+} 
+
+ui_page 'ui/dashboard.html'
+
+files {
+    'ui/img/*.png',
+    'ui/img/*.webp',
+    'ui/dashboard.html',
+    'ui/dmv.html',
+    'ui/bolos.html',
+    'ui/incidents.html',
+    'ui/penalcode.html',
+    'ui/reports.html',
+    'ui/warrants.html',
+    'ui/app.js',
+    'ui/style.css',
+}
